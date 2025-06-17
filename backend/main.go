39bbb4edb89b618440085f4dda8ef8b22e36d83d
christianrm0821 <christianrm0821@gmail.com>
2025-06-17 14:55:09 -0700

    index/css starting point, mux server, and respond with error and json functions

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,51 @@
 package main
 
-func main() {
-	/*
-		godotenv.Load()
-		dbURL := os.Getenv("DB_URL")
-		db, err := sql.Open("postgres", dbURL)
-		if err != nil {
-			log.Fatal("error: ", err)
-			return
-		}
-		mux := http.NewServeMux()
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	//"github.com/christianrm0821/happy/backend/structs"
+)
 
-		//myServer :=
-	*/
+func respondWithError(w http.ResponseWriter, status int, message string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	generalErr := errorResponse{
+		Error: message,
+	}
+	res, _ := json.Marshal(generalErr)
+	w.Write(res)
+
+}
+
+func respondWithJSON(w http.ResponseWriter, status int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	res, err := json.Marshal(payload)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error marshalling response")
+		return
+	}
+	w.Write(res)
+}
+
+func main() {
+	const filePath = "."
+	const port = ":8080"
+
+	serveMux := http.NewServeMux()
+
+	serveMux.Handle("/", http.FileServer(http.Dir(filePath)))
+
+	myServeMux := &http.Server{
+		Addr:    port,
+		Handler: serveMux,
+	}
+	err := myServeMux.ListenAndServe()
+	if err != http.ErrServerClosed {
+		log.Fatal("Server error: ", err)
+	}
 
 }
